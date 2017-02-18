422d96aef8a4cf59356c3c0b59b8e6ce7410dd5a
Sandeep <sjnews@gmail.com>
2017-02-18 16:34:50 -0500

    X shows up -no function

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 	$('#circle_banana').click(function () {
 		console.log('Banana');
 		$('#circle_banana, #circle_grapes').addClass('hide_btn');
+		$('#X-close').addClass('btn_close_show');
 		$('#slide').addClass('info_slides_show');
 		$('#popIn').text('The Banana is a year-round favorite, great tasting, nutritious, inexpensive and comes hermetically sealed.').addClass('popIn-info');
 	});
