f245d01aeaac984805635db22425204672064516
TC McCarthy <tc.mccarthy1@gmail.com>
2017-02-19 10:42:29 -0500

    More comments in the JS

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
 $(document).ready(function () { // we must wait for the DOM to be ready as the browser can take a little while to read all of the HTML and build the elements
-	/*function used in several places to show slide info*/
-	var showInfo = function () {
-		$('.circle').addClass('hide_btn'); /*.circle so it closes all circles.*/
-		$('#X-close').addClass('btn_close_show');
-		$('#slide').addClass('info_slides_show');
-	}
 
 	$(".circle").on("click", function (e) {
 		e.preventDefault(); //have to add this so we don't get weird behavior in older browsers (or IE)
 		var circle_clicked = $(this),
 			text = circle_clicked.data("text"); //looks for the data-text attribute and stores its value
 
-		$("#popIn").text(text);
 
-		$("#slide").addClass("active");
+		$("#popIn").text(text); //assign the text for the element to the popIn container in the slide
+
+		$("#slide").addClass("active"); //add a class="active" to #slide to apply the #slide.active styles to it, making it visible
 	});
 
 	$(".btn_close").on("click", function (e) {
 		e.preventDefault();
-		$("#slide").removeClass("active");
+		$("#slide").removeClass("active"); //remove class="active" from #slide, making it not visible
 	});
 });
