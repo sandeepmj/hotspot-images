ca3497f2df113e2fdc6511c599d1ae4ca76b32ba
Sandeep <sjnews@gmail.com>
2018-03-07 09:12:50 -0500

    added variables at the top

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+//declare variables
+var circle_clicked, // capture the circle that was clicked
+	var text, //capture the data-text attribute for circle that was circle_clicked
+		var popUp, //become the info_slide inside the closest photo-container div
+			var popUp_text, // find the text container inside the info_popIn div
+				var close_button_clicked; // capture which X is clicked 
+
 $(document).ready(function () { // we must wait for the DOM to be ready as the browser can take a little while to read all of the HTML and build the elements
 
 	$(".circle").on("click", function (e) {
 		e.preventDefault(); //have to add this so we don't get weird behavior in older browsers (or IE)
 
-		var circle_clicked = $(this), // capture the circle that was clicked on into a variable
+		circle_clicked = $(this), // capture the circle that was clicked on into a variable
 			text = circle_clicked.data("text"), //looks for the data-text attribute and stores its value
 			/*
 				Traverse DOM to the closest .photo-container ancestor and then
@@ -25,7 +32,7 @@ $(document).ready(function () { // we must wait for the DOM to be ready as the b
 		e.preventDefault();
 
 		//figure out which popup is open
-		var close_button_clicked = $(this),
+		close_button_clicked = $(this),
 			popUp = close_button_clicked.closest(".info_slides");
 
 		//remove the active class from the triggered popup
